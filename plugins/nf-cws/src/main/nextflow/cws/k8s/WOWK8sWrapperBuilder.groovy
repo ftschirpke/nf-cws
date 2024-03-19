a81521bcd23e703c12c28840c5b1f07f7cf4390f
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2024-03-19 14:19:02 +0100

    fix: remove init container code from wrapper builder

--- plugins/nf-cws/src/main/nextflow/cws/k8s/WOWK8sWrapperBuilder.groovy
+++ plugins/nf-cws/src/main/nextflow/cws/k8s/WOWK8sWrapperBuilder.groovy
@@ -71,23 +71,7 @@ class WOWK8sWrapperBuilder extends BashWrapperBuilder {
     @Override
     protected Map<String, String> makeBinding() {
         final Map<String,String> binding = super.makeBinding()
-        if ( binding.stage_inputs && storage && localWorkDir && !storage.withInitContainers() && !storage.separateCopy() ) {
-            final String cmd = """\
-                    # copy inputs from other nodes
-                    local s="\$PWD"
-                    cd "${workDir.toString()}"
-                    ${storage.getCmd().strip()} ${isTraceRequired()} &> ${WOWTaskRun.CMD_INIT_LOG}
-                    status=\$?
-                    if [ ! \$status -eq 0 ]; then
-                        echo "Error: ${storage.getCmd()} failed with status \$status"
-                        touch .command.init.failure
-                        exit 123
-                    fi
-                    cd "\$s" 
-            """.stripIndent()
-            binding.stage_inputs = cmd + binding.stage_inputs
-        }
-        if ( binding.stage_inputs && storage && localWorkDir && storage.separateCopy() ) {
+        if ( binding.stage_inputs && storage && localWorkDir ) {
             final String cmd = """\
                     # create symlinks
                     if test -f "${workDir.toString()}/.command.symlinks"; then
